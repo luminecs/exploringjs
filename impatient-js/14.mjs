9021fd1202c88d3514606e5be6baee2eae649bca
luminecs <wangxuke.china@gmail.com>
2024-01-02 21:25:32 +0800

    commit

--- impatient-js/14.mjs
+++ impatient-js/14.mjs
@@ -16,3 +16,22 @@ assert.equal(obj.unknownProp, undefined)
 function func1 () {}
 
 assert.equal(func1(), undefined)
+
+console.log(Object.getPrototypeOf(Object.prototype)) // null
+
+console.log(/a/.exec('x')) // null
+
+console.log(JSON.stringify({ a: undefined, b: null })) // {"b":null}
+
+let x = null
+if (x === null) {}
+x = undefined
+if (x === undefined) {}
+if (x !== undefined && x !== null) {}
+if (x) { // truthy?
+         // x is neither: undefined, null, false, 0, NaN, ''
+}
+if (x === undefined || x === null) {}
+if (!x) { // falsy?
+  // x is: undefined, null, false, 0, NaN, ''
+}
